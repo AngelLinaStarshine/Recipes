c304bc04435b81d298e69ef568dd382a9a7ca1da
Lina Di <deniagaangelina@gmail.com>
2025-01-04 02:32:04 -0500

    Update app

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import video from './food.mp4';
 import MyRecipesComponent from './MyRecipesComponent';
 
 
+
 function App() {
 
 const MY_ID = "35cf2e12";
 const MY_KEY = "35f0274d08cc569c0044f18ab903f7df";
 
 const [mySearch, setMySearch] = useState("");
-const [myRecipes, setMyRecipes] = useState([])
+const [myRecipes, setMyRecipes] = useState([]);
+const [wordSubmit, setWordSubmit] = useState("lemon");
 
 useEffect(() => {
   const getRecipe = async () => {
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=user&q=avocado&app_id=${MY_ID}&app_key=${MY_KEY}`);
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=$${wordSubmit}&app_id=${MY_ID}&app_key=${MY_KEY}`);
     const data = await response.json();
     setMyRecipes(data.hits);
   }
   getRecipe()
-}, [])
+}, [wordSubmit])
 
 
 const myRecipeSearch = (e) => {
   setMySearch(e.target.value)
 }
 
+const finalSearch = (e) => {
+  e.preventDefault()
+  setWordSubmit(mySearch)
+}
   return (
     <div className="App">
       <div className="container">
@@ -36,22 +42,24 @@ const myRecipeSearch = (e) => {
          </div>
 
          <div className='container'>
-          <form>
+          <form onSubmit={finalSearch}>
             <input className="search" onChange={myRecipeSearch} value={mySearch}/>
           </form>
           </div>
 
           <div className='container'>
-            <button>
+            <button onClick={finalSearch}>
               <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt="food"/>
             </button>
           </div>
 
-{myRecipes.map(element =>(
-  <MyRecipesComponent/>
-) 
-
-)}
+{myRecipes.map((element, index) =>(
+  <MyRecipesComponent key={index}
+  label={element.recipe.label} 
+  image={element.recipe.image} 
+  calories={element.recipe.calories}
+  ingredients={element.recipe.ingredientLines}/>
+) )}
 
 
     </div>
--- src/MyRecipesComponent.js
+++ src/MyRecipesComponent.js
@@ -1,7 +1,24 @@
-function MyRecipesComponent() {
+import check from './check.png';
+
+function MyRecipesComponent({label, image, calories, ingredients}) {
     return(
         <div>
-            <h2>Test</h2>
+            <div className="container">
+            <h2>{label}</h2>
+            </div>
+            <div className="container">
+            <img src={image} alt="recipe" width="300px"/>
+            </div>
+            <ul className="container list">
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>
+                        <img src={check} className="icon" alt="icon"/>
+                        {ingredient}</li>
+                ))}
+            </ul>
+            <div className="container">
+            <p>{calories.toFixed()} calories</p>
+            </div>
         </div>
     )
 }
